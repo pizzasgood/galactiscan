34926aac332d3aef8bcc9456f57d2db62484c46e
pizzasgood <pizzasgood@gmail.com>
2013-04-16 14:55:19 -0400

    Include version in status bar welcome message

--- gui.py
+++ gui.py
@@ -367,7 +367,7 @@ class Galactiscan(wx.Frame):
         self.status = wx.StatusBar(panel)
         self.status.SetFieldsCount(1)
         self.status.SetStatusStyles([wx.SB_FLAT])
-        self.status.SetStatusText("Welcome to %s" % (version.fancy_name))
+        self.status.SetStatusText("Welcome to %s" % (version.fancy_string))
         main_vbox.Add(self.status, proportion=0, flag=wx.EXPAND, border=0)
 
 
